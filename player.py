524e4b70f715afbb1d5fe3d6db2d6d4fb0d6c623
wrhm <bmatchen@gmail.com>
2017-03-21 00:11:18 -0400

    found resources for loud interval detection

--- player.py
+++ player.py
@@ -1,4 +1,14 @@
 # https://www.daniweb.com/programming/software-development/threads/491663/how-to-open-and-play-mp3-file-in-python
+'''
+TODOs
+	- https://www.pygame.org/docs/ref/mixer.html
+	- https://www.pygame.org/docs/ref/music.html#pygame.mixer.music.get_volume
+	- http://stackoverflow.com/questions/30571955/get-playing-wav-audio-level-as-output
+		- use rms to get loud intervals
+		- do this in preprocessing, then use music.get_pos to see if near a
+		  loud interval. if so, spit out high voltage to lights
+'''
+
 ''' pg_playmp3f.py
 play MP3 music files using Python module pygame
 pygame is free from: http://www.pygame.org
@@ -18,6 +28,10 @@ def play_music(music_file, volume=0.8):
     pg.mixer.init(freq, bitsize, channels, buffer)
     # volume value 0.0 to 1.0
     pg.mixer.music.set_volume(volume)
+    f = open('%s.txt'%(music_file.split('/')[-1].split('.')[0]),'wb')
+    soundObj = pg.mixer.Sound(music_file)
+    f.write(pg.mixer.Sound.get_raw(soundObj))
+    f.close()
     clock = pg.time.Clock()
     try:
         pg.mixer.music.load(music_file)
@@ -32,8 +46,8 @@ def play_music(music_file, volume=0.8):
 # pick a MP3 music file you have in the working folder
 # otherwise give the full file path
 # (try other sound file formats too)
-music_file = "meseeks.mp3"
+# music_file = "meseeks.mp3"
 music_file = "Machu_Picchu_-_The_Strokes_OFFICIAL_ALBUM_VERSION_.wav"
 # optional volume 0 to 1.0
 volume = 0.8
-play_music(music_file, volume)
+play_music('../AudioFiles/%s'%music_file, volume)
